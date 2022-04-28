f991600f113bb2b308a8a6aa280e14987c9e4e2f
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-04-28 17:32:19 -0400

    Initial-Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask, request, jsonify
+from openAI import gpt3, append_interaction_to_chatlog
+from TextSpeech import text_to_speech
+from SpeechText import speech_to_text
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def hello_world():
+    return 'This is a test...'
+
+@app.route('/getresponse', methods=["POST"])
+def voicebot_response(speech_input):
+    text_input = speech_to_text(speech_input)
+
+    openAI_response = gpt3(text_input)
+
+    speech_output = text_to_speech(openAI_response)
+    
+
+
+    return speech_output
+
