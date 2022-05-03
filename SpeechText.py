3c82f23e35e6b99401cc1c111cd6755112cdb1f8
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-05-02 22:51:16 -0400

    python-voice-input

--- SpeechText.py
+++ SpeechText.py
@@ -1,15 +1,23 @@
+from distutils.errors import UnknownFileError
 import speech_recognition as sr
 print(sr.__version__)
 
 def get_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+
+        r.adjust_for_ambient_noise(source)
+        print("please say something...")
+        print(r)
         audio = r.listen(source)
+        print("recognizing now...")
         said = ""
         
         try:
             said = r.recognize_google(audio)
             print(said)
+        except UnknownFileError as e:
+            print("yes: " + str(e))
         except Exception as e:
             print("Exception: " + str(e))
 
