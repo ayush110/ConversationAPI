53f0c4b5f2b848785dd8465ee82a51e131492854
ayush110 <67121244+ayush110@users.noreply.github.com>
2022-05-02 22:51:07 -0400

    pyhon-voice-input

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, request, jsonify
 from chatbot import gpt3, append_interaction_to_chatlog
 from TextSpeech import text_to_speech
-from SpeechText import speech_to_text
+from SpeechText import get_audio
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -14,7 +14,7 @@ def hello_world():
 @app.route('/getresponse', methods=["POST"])
 def voicebot_response(speech_input):
     
-    text_input = speech_to_text(speech_input)
+    text_input = get_audio(speech_input)
 
     openAI_response = gpt3(text_input)
 
